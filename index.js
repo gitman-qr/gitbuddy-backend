885986a9f0d0f437913e84dd5c656e6291bae42c
lishu <lishugupta652@gmail.com>
2022-04-01 00:04:30 +0530

    adding backend

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const env = require("dotenv").config();
+// Import routes
+const authRoutes = require("./routes/auth");
+const postRoutes = require("./routes/posts");
+
+// Connnect to Database using mongoose
+mongoose.connect(process.env.MONGODB_URI, () => {
+  console.log("Connected to Db");
+});
+
+// MiddleWare to use json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/app/user", authRoutes);
+app.use("/api/posts", postRoutes);
+// Index Route
+app.get("/", (req, res) => {
+  res.send({
+    msg: "Hello there this app is working totally fine",
+    route: "/app/user/register",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,64 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const { loginValidation, registerValidation } = require("../validation");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+router.get("/register", (req, res) => {
+  console.log("Reached Route");
+  res.send("try to send a post request");
+});
+router.post("/register", async (req, res) => {
+  // Validate the data
+  const { error, value } = registerValidation(req.body);
+  if (error) res.status(400).send(error.details[0].message);
+
+  // Check if the user already exists
+  const emailExists = await User.findOne({ email: req.body.email });
+  if (emailExists) return res.status(400).send("Email already exists");
+
+  // Hash the password
+  // It took me a while to figure out how to do this.
+  // Salt must be integer and bcrypt is responding with a string
+  const salt = bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, parseInt(salt));
+
+  // Create New User
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send({ user: savedUser._id });
+  } catch (err) {
+    console.log(err);
+    res.status(404).send(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  // Validate the data
+  const { error, value } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const { email, password } = req.body;
+
+  // Validating the email
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return res.status(400).send("Email or password is wrong");
+  }
+
+  // Validating the passowrd
+  const validPass = await bcrypt.compare(password, user.password);
+  if (!validPass) {
+    return res.status(400).send("Email or password is wrong");
+  }
+
+  // Create a token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.header("auth-token", token).send(token);
+});
+
+module.exports = router;
